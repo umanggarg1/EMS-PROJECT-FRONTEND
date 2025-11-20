726570be536dc9a7e385d65d5808a33d83d8d8d3
umanggarg1 <umanggarg9068@gmail.com>
2025-11-20 19:44:30 +0530

    Update existing files

--- ems-project/src/components/Dashboard/EmployeeDashboard.jsx
+++ ems-project/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from '../other/Header'
 import TaskBlock from '../other/TaskBlock'
+import TaskListBlock from '../TaskList/TaskListBlock'
 
 const EmployeeDashboard = () => {
   return (
-    <div className='p-10'>
+    <div className='p-10 bg-[#1C1C1C] h-screen'>
       {/* EmployeeDashboard  */}
-      <Header />
-      <TaskBlock />
+      <Header changeUser={props.changeUser} data={props.data}/>
+      <TaskBlock data={props.data}/>
+      <TaskListBlock data={props.data}/>
       
     </div>
   )
